ae8fa141d90742f2f7eb2f3973e7d95f3d54b1b5
Victor Cuellar <victorcuellar.parra@hotmail.com>
2023-06-13 23:52:29 +0200

    ProyectFinalReact

---
+++ src/components/results/Results.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+
+const Results = () => {
+  const [correctAnswers, setCorrectAnswers] = useState(0);
+
+  // Resto del código del componente...
+
+  return (
+    <div>
+      {/* Resto del contenido del componente */}
+    </div>
+  );
+};
+
+export default Results;
